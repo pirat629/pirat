36334239d3a47cbd347de18340e64da97a2de5df
pirat629 <pirat629@bk.ru>
2022-05-27 15:41:47 +0300

    Добавил комментарий

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // как дела
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
